89ca8343389e44ef67212ee6128fd13350c9e18b
mohamedgalia <mohamed.galia@mail.huji.ac.il>
2021-01-19 19:22:22 +0200

    add frontend

---
+++ frontend/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+from datetime import datetime
+
+api_key = "85aa5a4fb3533fbae7223f74ccb1befb"
+url = "http://data.fixer.io/api/latest?access_key=" + api_key
+
+app = Flask(__name__)
+info_list =[]
+when_get = dict()
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        input_json = request.get_json(force=True)
+        global when_get
+        when_get= input_json
+    return render_template("index2.html", info=when_get)
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug=True)
