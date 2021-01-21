c3da09f6c9bb23d3b7baa86ef72887cf88009a6a
mohamedgalia <mohamed.galia@mail.huji.ac.il>
2021-01-21 03:18:30 +0200

    reset

--- frontend/main.py
+++ frontend/main.py
@@ -9,11 +9,6 @@ import urllib, json
 import urllib.request
 import socket
 
-# ip_address = (requests.get("http://169.254.169.254/latest/meta-data/local-ipv4").content).decode('utf-8')
-# ip_address = '0.0.0.0'
-# ur1 = "http://" + ip_address + ":5000"
-# ur2 = "http://" + ip_address + ":5000/Auti/"
-
 api_key = "85aa5a4fb3533fbae7223f74ccb1befb"
 url = "http://data.fixer.io/api/latest?access_key=" + api_key
 
@@ -23,11 +18,15 @@ info_list =[]
 when_get = dict()
 @app.route("/", methods=["POST", "GET"])
 def index():
+    ip_address = (requests.get("http://169.254.169.254/latest/meta-data/public-ipv4").content).decode('utf-8')
+    # ip_address = '0.0.0.0'
+    ur1 = "http://" + ip_address + ":5000"
+    ur2 = "http://" + ip_address + ":5000/Auti/"
     if request.method == "POST":
         input_json = request.get_json(force=True)
         global when_get
         when_get = input_json
-    return render_template("index2.html", info=when_get)
+    return render_template("index2.html", info=when_get, val = ur1, val2 = ur2)
 
 
 if __name__ == "__main__":
